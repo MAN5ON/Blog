d7b2f31b295f81e0f193ad80b4a68d66d781ca99
MAN5ON <dark34rus@mail.ru>
2022-12-26 22:38:56 +0300

    feat(blog): add new post form

--- frontend/src/components/main/blog/index.jsx
+++ frontend/src/components/main/blog/index.jsx
@@ -1,12 +1,16 @@
-import { useState, React } from "react";
+import { React } from "react";
+import { Link } from "react-router-dom";
 import s from "../../styles/blog.module.css";
 import { BlogItem } from "./blogItem";
+import { NewPostItem } from "./newPostItem";
 
 export const Blog = () => {
 
 
   return (
     <div className={s.blogPage}>
+      <Link to="/new" ><button className={s.newBlogButton}>NEW POST</button></Link>
+      
       <BlogItem />
     </div>
   );
--- frontend/src/components/main/forum/messageItem.jsx
+++ frontend/src/components/main/forum/messageItem.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "../../styles/forum.module.css";
 import { useDispatch } from "react-redux";
-import { deleteMessage, updateMessage } from "../../redux/forumSlice";
+import { deleteMessage, updateMessageStatus } from "../../redux/forumSlice";
 
 export const MessageItem = ({ id, text, date, updateStatus }) => {
+  const [message, updateMessage] = useState(`${text}`);
+
   const dispatch = useDispatch();
+  const updateMessageInForum = () => {
+    if ((updateStatus = true)) {
+      console.log(typeof(message))
+      if (message.trim().length) {
+        dispatch(updateMessage({ id, message }));
+      }
+    } else {
+      dispatch(updateMessageStatus({ id }))
+    }
+  };
 
   return (
     <div className={s.chatMessage}>
@@ -17,18 +29,23 @@ export const MessageItem = ({ id, text, date, updateStatus }) => {
           ❌
         </button>
       </div>
-      {updateStatus === false ?
+      {updateStatus === false ? (
         <div className={s.message}>{text}</div>
-       :
-       <textarea className={s.message}>{text}</textarea>
-       }
-      
+      ) : (
+        <textarea
+          className={s.message}
+          value={message}
+          onChange={(e) => {
+            updateMessage(e.target.value);
+          }}
+        />
+      )}
 
       <div className={s.basementMessage}>
         <div className={s.dateMessage}>{date}</div>
         <button
           className={s.likes}
-          onClick={() => dispatch(updateMessage({ id }))}
+          onClick={updateMessageInForum}
         >
           🖊️
         </button>
